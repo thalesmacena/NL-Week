58ed611c93046f96e8ff22d391dff0a2d5ec8a6d
Thales Macena <thalesmacena@gmail.com>
2020-10-01 18:15:33 -0300

    Configuração do Servidor e de links das páginas

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
-require('express')()
+const express = require('express')
+const server = express()
+
+server.use(express.static("public"))
+
 .get("/", (req, res) => {
-  return res.send("index.html")
+  return res.sendFile(__dirname + "/views/index.html")
 })
-.get("/study", (req,res) => {
-  return res.send("study.html")
+.get("/study/", (req,res) => {
+  return res.sendFile(__dirname + "/views/study.html")
 })
 .get("/give-classes", (req,res) => {
-  return res.send("give-classes.html")
+  return res.sendFile(__dirname + "/views/give-classes.html")
 })
 .listen(5500)
