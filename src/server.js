ed91bc0636db95722f461abb93424e634d5fd111
Thales Macena <thalesmacena@gmail.com>
2020-10-04 21:39:27 -0300

    Configuração do nunjucks na página Study

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const proffys = [
   { name: "Thales Monteiro",
     avatar: "https://avatars1.githubusercontent.com/u/20719947?s=460&u=af34a508140033bbad586c34d8bfa3745b194fe2&v=4", 
     whatsapp: "21123456789", 
-    bio: "Apaixonado por tornar ideias realizadas.<br><br>Seu maior sonho é desenvolver uma IA de si mesmo para nunca mais passar raiva.", 
+    bio: "Apaixonado por tornar ideias realizadas. Seu maior sonho é desenvolver uma IA de si mesmo para nunca mais passar raiva.", 
     subject: "Ciência da Computação", 
-    cost: "20", 
+    cost: "20,00", 
     weekday: [0,6], 
     time_from: [720], 
     time_to: [1220]
